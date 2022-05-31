dfe37e39e3a910f84134787fdfef2575b7e0501a
Yurii Khimka <jw.artwork.design@gmail.com>
2022-05-31 16:16:15 +0300

    add value and popular sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//header__background
 const scrollHeader = () => {
 
     const header = document.getElementById('header');
@@ -8,3 +9,49 @@ const scrollHeader = () => {
 }
 
 window.addEventListener('scroll', scrollHeader);
+
+
+//swiper
+const swiperPopular = new Swiper(".popular__container", {
+
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: true,
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+//accordion
+const accordionItems = document.querySelectorAll('.value__accordion-item');
+
+accordionItems.forEach((item) => {
+    const accordionHeader = item.querySelector('.value__accordion-header');
+
+    accordionHeader.addEventListener('click', ()=> {
+
+        const openItem = document.querySelector('.accordion-open');
+
+        toggleItem(item)
+
+        if (openItem && openItem !== item) {
+            toggleItem(openItem)
+        }
+    });
+});
+
+const toggleItem = (item) => {
+    const accordionContent = item.querySelector('.value__accordion-content');
+
+    if (item.classList.contains('accordion-open')) {
+        accordionContent.removeAttribute('style')
+        item.classList.remove('accordion-open')
+    } else {
+        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        item.classList.add('accordion-open')
+    }
+}
